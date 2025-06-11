4808b91ff2ce942211858c54ffb36bc307cd7953
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-06-12 01:34:43 +0500

    fix: fix link

--- src/shared/config/apiClient.ts
+++ src/shared/config/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://xn----92-53d6cjmsd6amk0d.xn--p1ai/api",
+  baseURL: "https://xn----92-53d6cjmsd6amk0d.xn--p1ai/api/api",
   withCredentials: true,
 });
